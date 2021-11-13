ae96a72f087fd5f6d1439dfbedafc88fcafc83a7
Michael Allen Smith <digitalcolony@gmail.com>
2021-11-12 17:14:53 -0800

    move state data out of app code

---
+++ src/States.js
@@ -0,0 +1,47 @@
+const states = [
+	"PA",
+	"OR",
+	"CA",
+	"WA",
+	"MD",
+	"CA",
+	"MO",
+	"OR",
+	"WY",
+	"IL",
+	"ID",
+	"CT",
+	"WI",
+	"WA",
+	"MI",
+	"PA",
+	"SC",
+	"WA",
+	"NY",
+	"KS",
+	"OR",
+	"MT",
+	"CA",
+	"OR",
+	"WI",
+	"CO",
+	"ME",
+	"CA",
+	"CA",
+	"IN",
+	"WA",
+	"CA",
+	"OR",
+	"WI",
+	"OH",
+	"TX",
+	"NJ",
+	"MA",
+	"VT",
+];
+
+// Add states manually for now. Later pull in data from
+// Google Spreadsheet
+// // https://docs.google.com/spreadsheets/d/e/2PACX-1vQR37ZQny3aE5FtYXhqE8UwR4YGK6Z9A2iKeLEaCP8N_xvmlV5IO_W2s2rQWHXfHcUw6CBV-Q3O6OW0/pubhtml
+
+export { states };
