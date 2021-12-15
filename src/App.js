8033a4c05a4fbba09a903347941fab4fbae1036e
Michael Allen Smith <digitalcolony@gmail.com>
2021-12-15 11:25:37 -0800

    progress circle

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import USAMap from "react-usa-map";
 import "./App.css";
+import "./circle.css";
 import states from "./States";
 //import roasters from "./roasters.json";
 
@@ -35,6 +36,7 @@ class App extends Component {
 
 	statesCustomConfig = buildStatesConfig();
 	stateCount = getStateCount();
+	progressPercent = (this.stateCount / 51) * 100;
 
 	//roasterCustomConfig = buildRoastersConfig();
 
@@ -46,8 +48,21 @@ class App extends Component {
 					<p>My mission is to order great coffee online from every state (and DC). 😎</p>
 					<a href="https://docs.google.com/spreadsheets/d/1h-oqlqJ_G3UXuDSkdFHuEaCVuOXQOb68y2sduXQRTn4/edit?usp=sharing">
 						Coffee Roaster Spreadsheet
-					</a>
-					<p>Current progress: {this.stateCount}/51</p>
+					</a>{" "}
+					: Current progress: {this.stateCount}/51
+					<p></p>
+					<div className="progress">
+						<div className={`c100 p${22 + 33} orange`}>
+							<span>
+								{Math.round((this.stateCount / 51) * 100)}%
+								<br />
+							</span>
+							<div class="slice">
+								<div class="bar"></div>
+								<div class="fill"></div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		);
